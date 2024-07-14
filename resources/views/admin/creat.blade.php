982aa1f01854eb3617b48ef306a8804cbcdfcd0f
VovaPutin0 <Vitalystolyarov@icloud.com>
2024-07-14 11:52:44 +0300

    1

---
+++ resources/views/admin/creat.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts')
+@section('name')
+    Постник
+@endsection
+
+@section('content')
+<section>
+    <div class="container">
+        <h1 class="adminH1">Вы вошли как администратор сайта.</h1>
+        <a class="btn btn-outline-light" href="#">Написать рецепт</a>
+    </div>
+</section>
+@endsection
